483691773d3730e1cec73dc255b1850b3a849959
Yue Zhang <architectyuezhang@gmail.com>
2022-04-04 12:32:32 -0700

    remove files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyACdVlYx-a4k6sDZDx4xioyFz59QwJiaRg",
+    authDomain: "wesper-ff707.firebaseapp.com",
+    projectId: "wesper-ff707",
+    storageBucket: "wesper-ff707.appspot.com",
+    messagingSenderId: "836024878028",
+    appId: "1:836024878028:web:c4903af5f1c077fdc19369",
+    measurementId: "G-RLVH77QLWH"
+  };
